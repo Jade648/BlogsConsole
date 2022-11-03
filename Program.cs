ebcc4bf518bf70e1ffca30feb336a2038410c1f1
Jade 648 <maddiepeterson375@gmail.com>
2022-11-03 09:25:39 -0500

    updated Program file

--- Program.cs
+++ Program.cs
@@ -18,17 +18,17 @@ namespace BlogsConsole
 
                 Console.WriteLine("1) Display all Blogs");
 
-                Console.WriteLine(" 2) Add a Blog");
+                Console.WriteLine("2) Add a Blog");
 
                 Console.WriteLine("3) Create a Blog Post");
 
                 Console.WriteLine("4) Display all Posts");
 
-                Console.WriteLine(" Press q to exit the progam menu");
+                Console.WriteLine("Press q to exit the progam menu");
 
                 Console.ReadLine();
 
-                if (option == "3"){
+                 if (option == "3"){
 
                     Console.WriteLine("Select Blog to Post to");
 
@@ -46,36 +46,38 @@ namespace BlogsConsole
                     Console.ReadLine();
 
                     Console.WriteLine("Display all Posts and number of posts");
+    
                 }
 
-            } while (option == "3" | option == "4");
-
-             try
-            {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
+                   try
+                 {
+                    Console.Write("Enter a name for a new Blog: ");
+                
                 var name = Console.ReadLine();
 
-                var blog = new Blog { Name = name };
+                var blog = new Blog { Name = name };  
+                 
+                 var db = new BloggingContext();
 
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
+                  db.AddBlog(blog);
 
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
+                  var query = db.Blogs.OrderBy(b => b.Name);    
 
-                Console.WriteLine("All blogs in the database:");
+                     Console.WriteLine("All blogs in the database:");
                 foreach (var item in query)
                 {
                     Console.WriteLine(item.Name);
                 }
+
             }
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
             }
+
+            } while (option == "1" | option == "2");  
+
+            logger.Info("Program Ended");   
 
         }
     }
